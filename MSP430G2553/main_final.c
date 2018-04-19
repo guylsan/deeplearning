bb6e8f8b2b0b8dde5814f1efaaf78332a67dc7b6
Joubs93 <jean.joubert@hotmail.fr>
2018-04-19 14:23:41 +0200

    Update main_final.c

--- MSP430G2553/main_final.c
+++ MSP430G2553/main_final.c
@@ -9,30 +9,32 @@
 #define UltraRightPin BIT4
 #define UltraEcho BIT1
 
-volatile unsigned int measure; //this is the difference in counts measured by the Ultrasonic timer
+volatile unsigned int compteur; //this is the difference in counts measured by the Ultrasonic timer
 
 volatile unsigned int up=0; //helps the timer determine which edge
 
 unsigned int detection=0;
 
-//can these be switched to static within the timer?  they don't need to be available outside of the timer
-unsigned int measure_1 = 0;
-unsigned int measure_2 = 0;
+unsigned int attente=0;
 
-void Init_Timer0();
+unsigned int measure_cm; //distance mesuré par le capteur ultrason en centimètre
+
+unsigned char RX;		//Variable recu du raspberry
+
+
 void Attente_Timer0();
-void Init_Timer1(unsigned int vM ,unsigned int vA , unsigned int vB);
+void Init_Timer1(unsigned int vA , unsigned int vB);
 void Init_moteur();
-void Avancer_robot(unsigned int vM ,unsigned int vA ,unsigned int vB);
+void Avancer_robot(unsigned int vA ,unsigned int vB);
 void Acc_robot(unsigned int vM,unsigned int vA);
 void Desc_robot(unsigned int vM,unsigned int vA);
-void Tourner_Gauche_robot(unsigned int vM, unsigned int vA);
+void Tourner_Gauche_robot( unsigned int vA);
 void Rotation_90G_robot();
-void Tourner_Droite_robot(unsigned int vM, unsigned int vA);
+void Tourner_Droite_robot( unsigned int vA);
 void Rotation_90D_robot();
-void Reculer_robot(unsigned int vM, unsigned int vA);
+void Reculer_robot( unsigned int vA);
 void Stop_robot ();
-void Homologation (unsigned int vM , unsigned int vA);
+void Homologation ( unsigned int vA);
 void Init_Opto();
 
 
@@ -40,39 +42,47 @@ void Init_Opto();
  * Generation d'un timer de periode 0.1s f= 10Hz
  */
 
-/*
+
 void Init_Timer0(){
     BCSCTL1 = CALBC1_1MHZ; //frequence d’horloge 1MHz
     DCOCTL = CALDCO_1MHZ;
     TA0CTL |= ( TASSEL_2 | ID_0 | MC_1);
-    TA0CCR0 = 25000;
+    TA0CCR0 = 10;
     TA0CTL &= ~TAIFG;
 }
-*/
-
 void Attente_Timer0(){
-    Init_Timer0();
+
     while( !(TA0CTL & TAIFG));
     TA0CTL &= ~TAIFG;
 }
 
-
 /*
  * Fonction prenant vA et vB en parametre ce qui correspond a la vitesse du moteur A et Moteur B (vA et vB entre 0 et 8)
  */
-void Init_Timer1(unsigned int vM ,unsigned int vA , unsigned int vB){
-    BCSCTL1 = CALBC1_1MHZ; //frequence d’horloge 1MHz
-    DCOCTL = CALDCO_1MHZ;
+
+
+void Init_Timer1(unsigned int vA , unsigned int vB){
+     //frequence d’horloge 1MHz
+
     TA1CTL |= ( TASSEL_2 | ID_0 | MC_1);
     TA1CCTL1 |= OUTMOD_7;
     TA1CCTL2 |= OUTMOD_7;
-    TA1CCR0 = vM;
+    TA1CCR0 = 50000;	//50 ms
     TA1CCR1 = vA;
     TA1CCR2 = vB;
+    TA1CTL &= ~TAIFG;
 }
+void Attente_Timer1(){
+
+    while( !(TA1CTL & TAIFG));
+    TA1CTL &= ~TAIFG;
+}
+
+
 /*
  * Initialisation des fonction timer pour les moteurs et E/S pour le sens du moteur
  */
+
 void Init_moteur(){
     P2DIR |= ( BIT1 | BIT2 | BIT4 | BIT5 );
     P2SEL |= ( BIT2 | BIT4 );
@@ -81,9 +91,9 @@ void Init_moteur(){
 
 }
 //time en 0.1 sec
-void Avancer_robot(unsigned int vM ,unsigned int vA , unsigned int vB){
+void Avancer_robot(unsigned int vA , unsigned int vB){
 
-    Init_Timer1(vM,vA,vB);	// vB
+    Init_Timer1(vA,vB);	// vB
     Init_moteur();
     P2OUT |= ( BIT5 ); // Dans ce cas P2.5 = 1 et P2.1 = 0 avancer
     P2OUT &= ~( BIT1 );
@@ -91,59 +101,53 @@ void Avancer_robot(unsigned int vM ,unsigned int vA , unsigned int vB){
 }
 
 
-void Tourner_Gauche_robot(unsigned int vM, unsigned int vA){
-    Init_Timer1(vM,vA,vA);
+void Tourner_Gauche_robot( unsigned int vA){
+    Init_Timer1(vA,vA);
     Init_moteur();
     P2OUT |= ( BIT5 ); // Dans ce cas P2.5 = 1 et P2.1 = 0 avancer
     P2OUT |= ( BIT1 );
-    Attente_Timer0();
+
 }
 
 void Rotation_90G_robot(){
     int i;
     for ( i = 0 ; i<11 ; i++ ){
-        Tourner_Gauche_robot(105,20);
+        Tourner_Gauche_robot(40000);
     }
 }
 
-void Tourner_Droite_robot(unsigned int vM, unsigned int vA){
-    Init_Timer1(vM,vA,vA);
+void Tourner_Droite_robot( unsigned int vA){
+    Init_Timer1(vA,vA);
     Init_moteur();
     P2OUT &= ~( BIT5 ); // Dans ce cas P2.5 = 1 et P2.1 = 0 avancer
     P2OUT &= ~( BIT1 );
-    Attente_Timer0();
+
 }
 
 void Rotation_90D_robot(){
-    int i;
-    for ( i = 0 ; i<11 ; i++ ){
-        Tourner_Droite_robot(105,20);
-    }
+
+    Tourner_Droite_robot(40000);
+    __delay_cycles(450000);
+    Stop_robot ();
 }
 
-void Reculer_robot(unsigned int vM, unsigned int vA){
-    Init_Timer1(vM,vA,vA);
+void Reculer_robot( unsigned int vA){
+    Init_Timer1(vA,vA);
     Init_moteur();
     P2OUT &= ~( BIT5 ); // Dans ce cas P2.5 = 1 et P2.1 = 0 avancer
     P2OUT |= ( BIT1 );
-    Attente_Timer0();
+
 }
 
 void Stop_robot (){
     int i;
-    Init_Timer1(10,0,0);
+    Init_Timer1(0,0);
     Init_moteur();
     for( i = 0 ; i < 25 ; i++){
      __delay_cycles(5000);
     }
 
 }
-
-
-/*
- * Configuration for Trigger Pins - these drive the Ultrasonic device
- */
-
 
 void InitUART(void)
 {
@@ -163,25 +167,36 @@ void InitUART(void)
     /* Enable USCI_A0 RX interrupt */
     IE2 |= UCA0RXIE;
 }
+/*
+ * Fonction qui permet d'envoyer un trigger sur le port P1.4
+ */
 void get_measure() {
     UltraPortOut |= UltraFrontPin;
-    up = 1; //Next catch on Timer1A0 should be rising edge - helps with capture timer
+    up = 1;
     UltraPortOut &= ~UltraFrontPin;
 }
-//envoyer UART
-void TXdata( unsigned int c )
+/*
+ * Envoyer UART
+ */
+void TXdata( unsigned char c )
 {
     while (!(IFG2 & UCA0TXIFG));  // USCI_A0 TX buffer ready?
     UCA0TXBUF = c;              // TX -> RXed character
 }
 
-//recevoir UART
+/*
+ * Recevoir UART
+ */
 unsigned int UART_RXdata()
 {
 	while(!(IFG2 & UCA0RXIFG)); 				//receptionUSCI_A0 ? --wait flag
+	P1OUT ^= BIT0;
+	IFG2 &= ~UCA0RXIFG;
 	return UCA0RXBUF;
 }
-void clignottement_Led(int nb){
+
+// Clignottement de la lED Rouge
+void clignottement_Led_R(int nb){
 	unsigned int i;
 	for (i=0; i<(nb) ;i++){
 		P1OUT |= BIT0;
@@ -190,136 +205,180 @@ void clignottement_Led(int nb){
 		__delay_cycles(1000000);
 	}
 }
-
-// Clignottement de la lED en fonction du message recu du msp430
-void communication_UART_Led(){
-	unsigned int RX;
-	RX= UART_RXdata();
-	if ((RX & BIT1)==0 && (RX & BIT2)==0){
-		clignottement_Led(4);
-	}
-	if ((RX & BIT1)==2 && (RX & BIT2)==0){
-		clignottement_Led(3);
-	}
-	if ((RX & BIT1)==0 && (RX & BIT2)==4){
-		clignottement_Led(2);
-	}
-	if ((RX & BIT1)==2 && (RX & BIT2)==4){
-		clignottement_Led(1);
+// Clignottement de la lED Verte
+void clignottement_Led_V(int nb){
+	unsigned int i;
+	for (i=0; i<(nb) ;i++){
+		P1OUT |= BIT6;
+		__delay_cycles(1000000);
+		P1OUT &= ~BIT6;
+		__delay_cycles(1000000);
 	}
 }
+void test_reception(int test){
+	if (test != 0)
+		clignottement_Led_R(5);
+
+}
+/*
+ * Fonction traduction ascii
+ */
+unsigned int traduction_ascii(unsigned char c){
+	unsigned int valeur;
+	if (c=='a') valeur=1;
+	if (c=='b') valeur=2;
+	if (c=='c') valeur=3;
+	if (c=='d') valeur=4;
+	if (c=='e') valeur=0;
+	return valeur;
+}
+
+
+
+// Clignottement de la lED en fonction du message recu du Raspberry
+void communication_UART_Led(char detection){
+
+	P1OUT &= ~BIT0;
+	clignottement_Led_V(1);
+	clignottement_Led_R(detection);
+	clignottement_Led_V(2);
+}
+
+/*
+ * Fonction cerveau robot
+ */
 void pilote(){
+	unsigned int valeur;
 	Stop_robot ();
+	detection = BIT0;
+ 	TXdata(detection);
+	while(attente==0);
+	 __disable_interrupt();
+	detection = 0x00;
 	TXdata(detection);
-	
-	while((UART_RXdata() & BIT0)==0);
-	communication_UART_Led();
-	__delay_cycles(1000000);
+	P1OUT ^= BIT0;
+	valeur= traduction_ascii(RX);
+	communication_UART_Led(valeur);
+	//__delay_cycles(100000);
+
 	Rotation_90D_robot();
-	detection = 0;
-	TXdata(detection);
+	//Rotation_90G_robot();
+	__delay_cycles(500000);
+	Avancer_robot(50000,50000);
+	attente=0;
+	__enable_interrupt();
 }
 
 
 
 int main(void)
 {
-  WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
-  UltraPortDirection &= ~UltraEcho;
+   WDTCTL = WDTPW + WDTHOLD;
+  /*
+   * Initialisation des timer
+   */
+  Init_Timer0();
+  Init_Timer1(0,0);
 
-   //***Timer1A? capture configuration
-   //rising & falling edge + synchronous + P2.0 (CCI1A) + capture + capture/compare interrupt enable
-   TA0CCTL0 |= CM_3 + CCIS_0 + CAP + CCIE + SCS;
-   //select smclock for timer a source + make ta1ccr0 count continously up + no division
-  TA0CTL |= TASSEL_2 + MC_2 + ID_0;
+  IFG2 &= ~UCA0RXIFG;
 
-  //***Set up pins for Ultrasonic sensing
- UltraPortDirection |= BIT4;
- UltraPortOut &= ~BIT4;//turn off trigger pins to make sure they're in the correct state
- //Set P2.0 to pick up echo from the HC-SR04
- //Not using a #define element for this - it's tied to the timer
+  TA1CTL |= TAIE;   //Autorisation des interruptions sur le timer
+
+  UltraPortDirection |= BIT4; //Gestion du port P1.4 pour le trigger
+  UltraPortOut &= ~BIT4;  //turn off trigger pins
+
   P1SEL |= UltraEcho;
   P1SEL2 &= ~UltraEcho;
 
+  /*
+   * Gestion du port P1.5 pour la réception du signal d'echo et l'autorisation d'interruption sur celui-ci.
+   */
+  P1DIR &= ~BIT5;
+  P1IE |= BIT5;
+  P1IES &= ~BIT5;
+  P1IFG &= ~BIT5;
 
+  /*
+   * Gestion des horloges de l'UART et baudrate
+   */
   BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
   DCOCTL = CALDCO_1MHZ;
   UCA0CTL1 |= UCSSEL_2;                     // SMCLK
   UCA0BR0 = 104;                            // 1MHz 9600
   UCA0BR1 = 0;                              // 1MHz 9600
 
-  P1DIR &= ~BIT3;
-  P1IE |= BIT3;
-  P1OUT |= BIT3; // pull-up
+  /*
+   * Bouton poussoir
+   */
+  P1DIR &= ~BIT3;	// Port P1.3 en entrée
+  P1IE |= BIT3;		// Interruption sur le bouton poussoir
+  P1OUT |= BIT3;    // pull-up
   P1REN |= BIT3;	// activer la résistance
   P1IES &= ~BIT3;
-  P1IFG &= ~(BIT3);
-  P1DIR |= BIT0;
-  P1OUT &= ~BIT0;
+  P1IFG &= ~(BIT3);	//flag a 0
 
+  /*
+   * LEDs
+   */
+  P1DIR |= (BIT0|BIT6);
+  P1OUT &= ~(BIT0|BIT6);
+
+  InitUART();
   __enable_interrupt();
 
-/*
- * 	Gestion de l'UART
- */
-	if(CALBC1_1MHZ==0xFF || CALDCO_1MHZ==0xFF)
-	{
-		__bis_SR_register(LPM4_bits); // Low Power Mode #trap on Error
-	}
-	else
-	{
-		// Factory parameters
-		BCSCTL1 = CALBC1_1MHZ;
-		DCOCTL = CALDCO_1MHZ;
-	}
-	
-  InitUART();
-  
-  
-  while (1) {
-      __delay_cycles(50000);
-       if (detection ==1)
+
+  while (1)
+
+  {
+	 // Avancer_robot(50000,50000);
+	   if (detection == 0x01)
     	   	  pilote();
-       get_measure();
-
-
+	   	   	 //  Avancer_robot(50000,50000);
   }
-}
-
-//Timer1_A Capture
-//P1.4 ends up triggering this timer
-
-
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer1A0(void)
-{
-   if(up==1) //is this the rising edge?
-   {
-      measure_1=TA0CCR0;  // take first time measure
-//      serialPrint("{");
-   }
-   else //is this the falling edge?
-   {
-      measure_2=TA0CCR0; //take second time measure
-      measure=(measure_2-measure_1)/58; // microseconds / 58 = centimeters
-
-      if (measure <20){
-    	  P1OUT |= BIT0;
-      }
-      else
-    	  P1OUT &= ~BIT0;
-
-   }
-   up=!up; //if this was the rising edge, the next one will be a falling edge, and vice-versa
-   TA0CTL &= ~TAIFG; //clear timer A interrupt flag, so the chip knows we handled the interrupt
 
 }
 
+
+#pragma vector=TIMER1_A1_VECTOR
+__interrupt void Timer1A0(void){
+   get_measure();
+    TA1CTL &= ~TAIFG;
+}
 #pragma vector = PORT1_VECTOR
 __interrupt void Port_1(void) {
 
-			Avancer_robot(100,100,100);
+	if (P1IFG & BIT5){
+	compteur=0;
+		while ((P1IN & BIT5)!=0){
 
+			compteur++;
+		}
+		measure_cm = (compteur*10)/58;
+		if (measure_cm <10){
+		    	  detection = 0x01;
+		    	  P1OUT |= BIT0;
+
+
+		      }
+		      else{
+		    	  P1OUT &= ~BIT0;
+		      }
+		P1IFG &= ~(BIT5);
+	}
+	if(P1IFG & BIT3)
+	{
+		Avancer_robot(50000,50000);
 		P1IFG &= ~(BIT3);
+	}
 
 }
+
+
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
+{
+RX = UART_RXdata();
+attente=1;
+}
+
+
